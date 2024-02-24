f39fbb2c427aa699fc03cf63c5bb03261cff2498
danish45007 <danish45007@gmail.com>
2024-02-24 19:41:09 +0530

    added third challenge (Efficient Broadcast, Part I) updated

--- efficient_broadcast_1/main.go
+++ efficient_broadcast_1/main.go
@@ -11,7 +11,6 @@ import (
 
 	"github.com/emirpasic/gods/trees/btree"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
-
 	log "github.com/sirupsen/logrus"
 )
 
@@ -27,9 +26,7 @@ import (
  n1 -> n2, n3
  n2 -> n1 // not required
  n3 -> n1 // not required
-
  n2 ->n3
-
  n2 -> n1
  n1 -> n3
 max distance = 2
@@ -38,85 +35,90 @@ max distance = 2
 */
 
 // MaxRetries is the maximum number of retries in case of n/w failure
-const MaxRetries = 100
+const MaxRetry = 100
 
 type server struct {
 	Node       *maelstrom.Node
-	NodeID     string
-	RootNodeID int
+	NodeId     string
+	RootNodeId int
+
 	// message
-	messageIDsLock sync.RWMutex
-	MessageIDs     map[int]struct{}
+	messageIdsLock sync.RWMutex
+	MessageIds     map[int]struct{}
 
 	// topology
 	topologyLock sync.RWMutex
 	Topology     *btree.Tree // flat tree (root -> [c1,c2....]): efficient data structure to store the topology of the network
 }
 
-// Logging the output to a file
 func init() {
-	file, err := os.OpenFile("./maelstrom.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	f, err := os.OpenFile("/tmp/maelstrom.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		panic(err)
 	}
-	log.SetOutput(file)
+	log.SetOutput(f)
 }
 
 func main() {
 	newNode := maelstrom.NewNode()
-	server := &server{
-		Node:       newNode,
-		MessageIDs: make(map[int]struct{}),
-	}
+	server := &server{Node: newNode, MessageIds: make(map[int]struct{})}
+
 	newNode.Handle("init", server.initHandler)
 	newNode.Handle("broadcast", server.broadcastHandler)
 	newNode.Handle("read", server.readHandler)
 	newNode.Handle("topology", server.topologyHandler)
+
+	if err := newNode.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 // initHandler initializes the server
 func (s *server) initHandler(_ maelstrom.Message) error {
-	s.NodeID = s.Node.ID()
+	s.NodeId = s.Node.ID()
 	// node with the lowest id is the root node
 	// nodeId is the node id without the prefix "n"
-	nodeId, err := strconv.Atoi(s.NodeID[1:])
+	nodeId, err := strconv.Atoi(s.NodeId[1:])
 	if err != nil {
 		return err
 	}
-	s.RootNodeID = nodeId
+	s.RootNodeId = nodeId
 	return nil
 }
 
 func (s *server) broadcastHandler(msg maelstrom.Message) error {
-	var responseMessage map[string]interface{}
-	// unmarshal the message
-	if err := json.Unmarshal(msg.Body, &responseMessage); err != nil {
+	var body map[string]any
+	// unmarshal the message body
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
 		return err
 	}
+
 	// return back the message in a separate go routine
 	go func() {
-		_ = s.Node.Reply(msg, map[string]interface{}{
+		_ = s.Node.Reply(msg, map[string]any{
 			"type": "broadcast_ok",
 		})
 	}()
+
+	id := int(body["message"].(float64))
+	s.messageIdsLock.Lock()
 	// check if the message has been seen
-	messageID := int(responseMessage["message"].(float64))
-	s.messageIDsLock.Lock()
-	if _, seen := s.MessageIDs[messageID]; seen {
-		s.messageIDsLock.Unlock()
+	if _, exists := s.MessageIds[id]; exists {
+		s.messageIdsLock.Unlock()
 		return nil
 	}
-	// mark it as seen
-	s.MessageIDs[messageID] = struct{}{}
-	s.messageIDsLock.Unlock()
+	// mark the message as seen
+	s.MessageIds[id] = struct{}{}
+	s.messageIdsLock.Unlock()
+
 	// broadcast the message to all other nodes
-	return s.broadcast(responseMessage, msg.Src)
+	return s.broadcast(msg.Src, body)
 }
 
-func (s *server) broadcast(message map[string]interface{}, srcNode string) error {
+func (s *server) broadcast(src string, body map[string]any) error {
 	s.topologyLock.RLock()
 	// root node for the flat tree
-	rootNode := s.Topology.GetNode(s.RootNodeID)
+	rootNode := s.Topology.GetNode(s.RootNodeId)
 	defer s.topologyLock.RUnlock()
 
 	var neighbors []string
@@ -125,32 +127,32 @@ func (s *server) broadcast(message map[string]interface{}, srcNode string) error
 		// append the parent node to the neighbors list
 		neighbors = append(neighbors, rootNode.Parent.Entries[0].Value.(string))
 	}
+
 	// append the children nodes to the neighbors list
 	for _, children := range rootNode.Children {
 		for _, entry := range children.Entries {
-			// append the value of the entry to the neighbors list
 			neighbors = append(neighbors, entry.Value.(string))
 		}
 	}
+
 	for _, dst := range neighbors {
-		if dst == srcNode || dst == s.NodeID {
+		if dst == src || dst == s.NodeId {
 			continue
 		}
-		// create a closure to capture the dst
+
 		dst := dst
-		// send the message to the dst
+		// send the message to the dst node
 		go func() {
-			if err := s.rpc(message, dst); err != nil {
+			if err := s.rpc(dst, body); err != nil {
 				// in case of n/w failure, retry sending the message
-				for i := 0; i < MaxRetries; i++ {
-					if err := s.rpc(message, dst); err != nil {
-						// sleep for 1 second before retrying
+				for i := 0; i < MaxRetry; i++ {
+					if err := s.rpc(dst, body); err != nil {
+						// Sleep and retry
 						time.Sleep(time.Duration(i) * time.Second)
 						continue
 					}
 					return
 				}
-				// log the error
 				log.Error(err)
 			}
 		}()
@@ -158,43 +160,46 @@ func (s *server) broadcast(message map[string]interface{}, srcNode string) error
 	return nil
 }
 
-func (s *server) rpc(message map[string]interface{}, dst string) error {
+func (s *server) rpc(dst string, body map[string]any) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	_, err := s.Node.SyncRPC(ctx, dst, message)
+	_, err := s.Node.SyncRPC(ctx, dst, body)
 	return err
 }
 
 func (s *server) readHandler(msg maelstrom.Message) error {
 	// get the list of all the seen message_ids
-	messageIDs := s.GetAllMessageIds()
-	return s.Node.Reply(msg, map[string]interface{}{
+	seenMessageIds := s.getAllIDs()
+
+	return s.Node.Reply(msg, map[string]any{
 		"type":     "read_ok",
-		"messages": messageIDs,
+		"messages": seenMessageIds,
 	})
 }
 
-func (s *server) GetAllMessageIds() []int {
-	s.messageIDsLock.RLock()
-	seenMessageIDs := make([]int, 0, len(s.MessageIDs))
-	for id := range s.MessageIDs {
-		seenMessageIDs = append(seenMessageIDs, id)
+func (s *server) getAllIDs() []int {
+	s.messageIdsLock.RLock()
+	ids := make([]int, 0, len(s.MessageIds))
+	for id := range s.MessageIds {
+		ids = append(ids, id)
 	}
-	s.messageIDsLock.RUnlock()
-	return seenMessageIDs
+	s.messageIdsLock.RUnlock()
+
+	return ids
 }
 
 func (s *server) topologyHandler(msg maelstrom.Message) error {
-	topology := btree.NewWithIntComparator(len(s.Node.NodeIDs()))
+	tree := btree.NewWithIntComparator(len(s.Node.NodeIDs()))
 	for i := 0; i < len(s.Node.NodeIDs()); i++ {
 		// add the node id to the topology
-		nodeWithPrefix := fmt.Sprintf("n%d", i)
-		topology.Put(i, nodeWithPrefix)
+		tree.Put(i, fmt.Sprintf("n%d", i))
 	}
+
 	s.topologyLock.Lock()
-	s.Topology = topology
+	s.Topology = tree
 	s.topologyLock.Unlock()
-	return s.Node.Reply(msg, map[string]interface{}{
+
+	return s.Node.Reply(msg, map[string]any{
 		"type": "topology_ok",
 	})
 }
